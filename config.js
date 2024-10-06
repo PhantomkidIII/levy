2110b66c2f2090f7299a4a5f85b3fb779fb4954e
PhantomkidIII <ashkings0100@gmail.com>
2024-10-06 15:13:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
           },
           logging: false,
         }),
-  HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
+  HANDLERS: (process.env.PREFIX || '^[!]').trim(),
   SUDO: process.env.SUDO || '2348120549697',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
